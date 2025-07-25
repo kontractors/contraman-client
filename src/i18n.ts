2e255e707a64b63b901250b3eab08f1671d09f3d
416rehman <416rehman@ahmadz.ai>
2025-07-24 20:41:41 -0400

    init with i18n

---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import en from "/locales/en.json?url";
+import fr from "/locales/fr.json?url";
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const supportedLngs = ['en', 'fr'];
+const resources = {
+    en: {translation: en},
+    fr: {translation: fr}
+};
+
+i18n.use(LanguageDetector).use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources,
+        // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+        // if you're using a language detector, do not define the lng option
+        fallbackLng: 'en',
+        supportedLngs,
+        interpolation: {
+            escapeValue: false // react already safes from xss
+        }
+    });
+
+export default i18n;
